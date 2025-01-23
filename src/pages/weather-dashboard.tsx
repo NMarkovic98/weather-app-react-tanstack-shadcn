2591a1fe2064f03b6212d379aac94c5b9834b690
nmarkovic <pomocnih@gmail.com>
2025-01-23 21:52:23 +0100

    feat(WA): add loading skeleton; add get geo location hook;

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,5 +1,56 @@
+import WeatherSkeleton from "@/components/loading-skeleton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { useGeolocation } from "@/hooks/use-geolocation";
+import { AlertTriangle, MapPin, RefreshCcw } from "lucide-react";
+
 const WeatherDashboard = () => {
-  return <div>Weather Dashboard</div>;
+  const {
+    coordinates,
+    error: locationError,
+    getLocation,
+    isLoading: locationLoading,
+  } = useGeolocation();
+
+  const handleRefresh = () => {
+    getLocation();
+    if (coordinates) {
+      // reload weather data
+    }
+  };
+
+  if (locationLoading) {
+    return <WeatherSkeleton />;
+  }
+
+  if (locationError) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription>
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant="outline" className="w-f">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  return (
+    <div className="space-y-4">
+      {/* Fav cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button variant="outline" size="icon" onClick={handleRefresh}>
+          <RefreshCcw className="h4 w-4" />
+        </Button>
+      </div>
+      {/* Current and Hourly weather */}
+    </div>
+  );
 };
 
 export default WeatherDashboard;
